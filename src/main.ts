8b136f96d3f61a977eae78768091f25963e40ed2
Oluwatobiloba <oluwateezzy03@gmail.com>
2024-11-14 09:29:55 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
     origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
